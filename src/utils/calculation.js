ce6cafd5142e170a52153a8e7f50398c1e8f7017
nitroks14 <43478520+nitroks14@users.noreply.github.com>
2026-05-11 15:46:33 +0200

    Add files via upload

--- src/utils/calculation.js
+++ src/utils/calculation.js
@@ -1,1 +1,6 @@
-
+export function calculateTime({ volume, flow, finalPressure, bends }) {
+  const P0 = 1000
+  const bendPenalty = Math.max(1 - bends * 0.03, 0.5)
+  const effectiveFlow = flow * bendPenalty
+  return (volume / effectiveFlow) * Math.log(P0 / finalPressure) * 3600
+}
